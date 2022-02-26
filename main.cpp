1e605964774503e042cea95350461429c47278e3
Don wijesinghe <wijey@MacBook-Pro-2.lan>
2022-02-26 10:04:05 -0600

    processed input file

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include "master_thread.h"
 #include<string>
-
+#include<sstream>
 using namespace std;
 
 void openfile(string, ifstream &);
@@ -32,9 +32,8 @@ int main(int argc, char *argv[]){
         exit(1);
     }
     
-    //process input file
-
-
+    //process input file : done
+    
     //launch master thread 
 
     //wait for master thread to finish 
@@ -49,11 +48,59 @@ void openfile(string filename,ifstream &infile ){
     //add protection
 }
 
-void processfile(ifstream &input){
-    //char* g;
-    //input.getline(g,256);
-    //int num_nodes; 
-    //num_nodes=(int)input.get();
-    //master.num_child_threads=num_nodes;
-    //cout << "num nodes is " << g[0] << endl;
+//try implementation with dynamic array later
+//(consider whitespace character to count ill use getline)
+
+//why I used >> instead of .get & .getline ? 
+//ignore whitespace incase user entered whitespace
+//disadvantage: manually get rid of newline 
+
+void processfile(ifstream &inputfile){
+    //store number of nodes in master variable one 
+
+    //store child thread uids in vector 
+
+    //store number of nodes
+    string str;
+    string token;
+    getline(inputfile,str);
+    master.num_child_threads=stoi(str);
+    
+    //store child thread info
+    getline(inputfile,str);
+    istringstream iss(str);
+   
+    while (getline(iss, token, ' '))
+    {
+        master.UID.push_back(stoi(token));
+    }
+
+    master.neighbor_UIDS.resize(master.num_child_threads);
+   
+    for(int i=0; i<master.num_child_threads;i++){
+        getline(inputfile,str);
+        istringstream is(str);
+        cout << str << endl;
+    
+        while (getline(is, token, ' '))
+        {
+            int x = stoi(token);
+            master.neighbor_UIDS[i].push_back(master.UID[x-1]);
+        }
+    }
+
+    for(int i =0; i<master.UID.size(); i++){
+        cout << master.UID[i] <<endl;
+    }
+
+    for(int i =0; i<master.UID.size(); i++){
+        cout << "UID is " << master.UID[i] << ". neighbors are : ";
+        for (int j=0; j<master.neighbor_UIDS[i].size(); j++){
+            cout << master.neighbor_UIDS[i][j] << " ";
+        }
+        cout <<endl;
+    }
+
+
+
 }
--- master_thread.h
+++ master_thread.h
@@ -1,8 +1,12 @@
 #ifndef master_thread
 #define master_thread
+#include <vector>
+
 
 struct {
     int num_child_threads;
+    std::vector<int> UID;
+    std::vector<std::vector<int> > neighbor_UIDS;
 }master;
 
 #endif
